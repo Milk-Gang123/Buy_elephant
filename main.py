0444fc26c416c17f8d4c4d0f79aa4e6e736f1227
Администратор <alexushveridze@mail.ru>
2022-03-18 21:34:28 +0300

    1234

--- main.py
+++ main.py
@@ -142,6 +142,7 @@ def handle_dialog(req, res):
         # Пользователь согласился, прощаемся.
         res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
         res['response']['end_session'] = True
+        res['response']['end_session'] = False
         WAS = 1
         return redirect('/post')
 
